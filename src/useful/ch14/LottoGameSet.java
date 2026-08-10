9e17460e502f69687dfcea7891cfa91773518ca2
근호 <insertvalue@naver.com>
2026-08-10 11:59:24 +0900

    List, Set 예제

---
+++ src/useful/ch14/LottoGameSet.java
@@ -0,0 +1,34 @@
+package useful.ch14;
+
+import java.util.*;
+
+public class LottoGameSet {
+
+    public static void main(String[] args) {
+
+        Set<Integer> lotto = new HashSet<>();
+        Random random = new Random();
+        int count = 0;
+        // (5)(1)(2)(3)(4)(9) // lotto.size() < 6 0 --> false
+        while (lotto.size() < 6) {
+            int number = random.nextInt(45) + 1;
+            lotto.add(number);
+            count++;
+        }
+        System.out.println("이번주 로또 번호 : " + lotto);
+        System.out.println("while 문 반복 횟수 : " + count);
+
+        System.out.println("---------------------------------------------------");
+        // ArrayList 생성자 안에 Set 계을 넣어서 List 계열에 객체를 생성할 수 있다.
+        List<Integer> sortedLotto = new ArrayList<>(lotto);
+        System.out.println(sortedLotto);
+        System.out.println("---------------------------------------------------");
+        // 오름 차순 정렬 (ArrayList )
+        Collections.sort(sortedLotto);
+        System.out.println("오름 차순 된 로또 번호 : " + sortedLotto);
+
+        // 도전 과제 - 내린 차순으로 정렬하는 코드를 작성하시오 (문서 api, 구글링, AI )
+
+
+    } // end of main
+}
